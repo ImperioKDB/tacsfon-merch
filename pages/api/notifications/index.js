2518f3ab453aea6a44716b9482e094188c27394d
ImperioKDB <kingbrite123@gmail.com>
2026-05-14 08:45:24 +0100

    fix(security): restore auth middleware on 11 unprotected API routes

--- pages/api/notifications/index.js
+++ pages/api/notifications/index.js
@@ -5,7 +5,6 @@
  * Supports optional ?unread_only=true query param.
  */
 import { withMiddleware } from '../../../lib/middleware/withMiddleware.js'
-import { authMiddleware } from '../../../lib/middleware/auth.js'
 import { sendSuccess }    from '../../../lib/responseFormatter.js'
 import { supabaseAdmin }  from '../../../lib/supabase.js'
 
@@ -40,4 +39,4 @@ async function handler(req, res) {
   })
 }
 
-export default withMiddleware(handler, [authMiddleware])
+export default withMiddleware(handler, { requireAuth: true })
